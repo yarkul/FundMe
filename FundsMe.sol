e5671c675fa531e5564a3a30d40224e97efb9518
yarkul <yarkul777@gmail.com>
2025-02-05 10:27:36 +0100

    Connected with Oracle

--- FundsMe.sol
+++ FundsMe.sol
@@ -1,3 +1,27 @@
-//Get fund from users
-//Withdraw funds
-//Set a minimum funding value in USD
+//SPD-License-Identifier: MIT
+
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+
+    uint256 public minimumUsdt = 5;
+
+    function fund() public payable  {
+        require (msg.value > minimumUsdt, "don't enougth ETH");
+    }
+
+    function getPrice() public view returns(uint256) {
+        //0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306); 
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+
+        return uint256(answer * 1e10);
+    }
+
+    function getPriceConversion() public {
+
+    }
+}
